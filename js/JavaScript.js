1ae973d9b65dd5b8fa6d1299abefd666716d83ab
Elomi Gire <elomi.gire.78@gmail.com>
2024-08-29 19:59:39 +0900

    tambah file

---
+++ js/JavaScript.js
@@ -0,0 +1,13 @@
+//single line comment
+
+/*
+multiple line commint
+bisa tulis juga di sini
+*/
+
+var namaDepan = "Elomi";
+var namaBelakang ="Gire";
+
+
+document.getElementById("nama")
+.innerHTML = namaDepan + namaBelakang;
